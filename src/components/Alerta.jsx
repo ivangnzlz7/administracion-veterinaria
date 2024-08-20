051d51d8795722954045fe3abfacce20e0f8174c
Ivan javier <ig6119838@gmail.com>
2024-08-19 21:29:27 -0300

    alerta check

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -2,10 +2,10 @@
 
 const Alerta = ({alerta}) => {
   return (
-    <div className={`${alerta.error ? 'from-red-600 to-red-600' : 'from-lime-500 to-lime-500'}
+    <div className={`${alerta.error ? 'from-red-600 to-red-600 dark:bg-red-700' : 'from-purple-800 to-purple-800 dark:bg-blue-900'}
     bg-gradient-to-br text-center p-3 rounded-xl uppercase text-white font-bold text-sm mb-10 `}>
 
-        {alerta.msg}
+        {alerta.msg} 
       
     </div>
     //'from-indigo-400 to-indigo-600'
